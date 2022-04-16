f0108ddb53302a765ffd43b20ed1be3cffc059bf
TiveCS <creativecode.id@gmail.com>
2022-04-16 11:26:48 +0700

    add Feedback model and schema

---
+++ BlueRecandy/Models/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueRecandy.Models
+{
+	public class Feedback
+	{
+
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string FeedbackContent { get; set; }
+
+		[Required]
+		[Range(1, 5)]
+		public int Rating { get; set; }
+
+		[Required]
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
+
+		[Required]
+		public string UserId { get; set; }
+		public ApplicationUser User { get; set; }
+
+	}
+}
