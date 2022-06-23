aa25f13526d627a3630ce0af541e6d7a509a5f5f
TiveCS <creativecode.id@gmail.com>
2022-06-24 01:06:14 +0700

    add user service

---
+++ BlueRecandy/Services/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using BlueRecandy.Models;
+using System.Security.Claims;
+
+namespace BlueRecandy.Services
+{
+	public interface IUsersService
+	{
+
+		Task<ApplicationUser> GetUserById(string id);
+
+		Task<ApplicationUser> GetUserByEmail(string email);
+
+		Task<ApplicationUser> GetUserByClaims(ClaimsPrincipal claim);
+
+	}
+}
---
+++ BlueRecandy/Services/UsersService.cs
@@ -0,0 +1,38 @@
+﻿using BlueRecandy.Data;
+using BlueRecandy.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace BlueRecandy.Services
+{
+	public class UsersService : IUsersService
+	{
+
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public UsersService(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
+
+		public async Task<ApplicationUser> GetUserByClaims(ClaimsPrincipal claim)
+		{
+			var result = await _userManager.GetUserAsync(claim);
+			return result;
+		}
+
+		public async Task<ApplicationUser> GetUserByEmail(string email)
+		{
+			var found = await _userManager.FindByEmailAsync(email);
+			return found;
+		}
+
+		public async Task<ApplicationUser> GetUserById(string id)
+		{
+			var found = await _userManager.FindByIdAsync(id);
+			return found;
+		}
+	}
+}
