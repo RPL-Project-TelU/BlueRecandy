6dae7c1a6837567c1c78db06d538b2f03dcc747e
TiveCS <creativecode.id@gmail.com>
2022-06-28 18:23:59 +0700

    Split test code into separate file based on method

---
+++ BlueRecandy.UnitTest/Services/ProductsService/GetProductByIdTest.cs
@@ -0,0 +1,50 @@
+﻿using BlueRecandy.Models;
+using BlueRecandy.Services;
+using Moq;
+using Xunit;
+
+namespace BlueRecandy.UnitTest.Services.ProductsService
+{
+	public class GetProductByIdTest
+	{
+
+		[Fact]
+		public void GetProductById_IdNotNull_ProductIsNotNull()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			mockService.Setup(m => m.GetProductById(1)).ReturnsAsync(product);
+			var service = mockService.Object;
+
+			// Act
+			var actionResult = service.GetProductById(product.Id);
+			var result = actionResult.Result;
+
+			// Assert
+			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public void GetProductById_IdIsNull_ProductIsNull()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			mockService.Setup(m => m.GetProductById(1)).ReturnsAsync(product);
+			var service = mockService.Object;
+
+			// Act
+			var actionResult = service.GetProductById(null);
+			var result = actionResult.Result;
+
+			// Assert
+			Assert.Null(result);
+		}
+
+	}
+}
---
+++ BlueRecandy.UnitTest/Services/ProductsService/ValidateProductTest.cs
@@ -0,0 +1,173 @@
+﻿using BlueRecandy.Models;
+using BlueRecandy.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlueRecandy.UnitTest.Services.ProductsService
+{
+	public class ValidateProductTest
+	{
+
+		[Fact]
+		public void ValidateProduct_HasProduct_IsValid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = true;
+			product.DownloadURL = "https://download.org/";
+			product.Price = 12;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(true);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void ValidateProduct_HasProduct_IsInvalid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = true;
+			product.DownloadURL = null;
+			product.Price = -1000;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(false);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ValidateProduct_UseExternalURL_IsValid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = true;
+			product.DownloadURL = "https://download.org/";
+			product.Price = 12;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(true);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void ValidateProduct_UseExternalURL_IsInvalid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = true;
+			product.DownloadURL = null;
+			product.Price = 12;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(false);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ValidateProduct_UseFile_IsValid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = false;
+			product.SourceFileName = "Myfile";
+			product.Price = 12;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(true);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void ValidateProduct_UseFile_IsInvalid()
+		{
+			// Arrange
+			var mockService = new Mock<IProductsService>();
+
+			var product = new Product();
+			product.Id = 1;
+			product.Name = "My Product";
+			product.Description = "My Description";
+			product.UseExternalURL = false;
+			product.SourceFileName = null;
+			product.Price = 12;
+			product.OwnerId = "userid";
+
+			mockService.Setup(m => m.ValidateProduct(product)).Returns(false);
+			var service = mockService.Object;
+
+			// Act
+
+			var result = service.ValidateProduct(product);
+
+			// Assert
+			Assert.False(result);
+		}
+
+	}
+}
