7b2a7009115e17f7fdbbc6fba8eeeaf70f060cc2
luttfimohamad <luttfimohamad@gmail.com>
2022-07-02 09:01:13 +0700

    add Index and Privacy unit testing in homeControllertest

---
+++ BlueRecandy.UnitTest/Controller/HomeControllerTest/HomeControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using BlueRecandy.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlueRecandy.UnitTest.Controller.HomeControllerTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult()
+        {
+            // Assert
+            var mockService = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(mockService.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy_ReturnsAViewResult()
+        {
+            // Assert
+            var mockService = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(mockService.Object);
+
+            // Act
+            var result = controller.Privacy();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
